5316bdba214e473ac0c1c16da89a8fed4f07a963
Sharonne24 <sawuor395@gmail.com>
2023-10-02 23:47:29 +0300

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ApplicationRecord
     has_many :donations, as: :donorable
     has_many :applications, as: :applicantable
 
-    
+     #validations
+     validates :email, presence: true, uniqueness: true
+     validates :firstname, presence: true
+     validates :secondname, presence: true
+     validates :role, presence: true
 end
