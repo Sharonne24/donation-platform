a3c466ed468b5bec9987b061ead9585c8b9668ec
Sharonne24 <sawuor395@gmail.com>
2023-10-02 23:01:05 +0300

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,6 @@
 class UsersController < ApplicationController
   def new
-    # @user = User.new
-    render json: {message: "hello"}
+     @user = User.new
   end
 
-#   def create
-#     @user = User.new(user_params)
-#     if @user.save
-#       redirect_to login_path, notice: 'Registration successful. You can now log in.'
-#     else
-#       render :new
-#     end
-#   end
-
-#   private
-
-#   def user_params
-#     params.require(:user).permit(:email, :password, :firstname, :secondname, :role)
-#   end
 end
