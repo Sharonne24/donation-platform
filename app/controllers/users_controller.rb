7a71c015bbb83295e1d913db22fd60f2799e7514
Sharonne24 <sawuor395@gmail.com>
2023-10-02 23:28:38 +0300

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-     
+      redirect_to login_path, notice: 'Registration successful. You can now log in.'
+    else
+      render :new
     end
   end
 end
