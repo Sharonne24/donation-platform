247b26ca8ee2016d058ba5087cc564f0081fe562
Sharonne24 <sawuor395@gmail.com>
2023-10-03 00:06:46 +0300

    update

---
+++ db/migrate/20231002210036_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[7.0]
+  def change
+      add_column :users, :email, :string
+      add_column :users, :firstname, :string
+      add_column :users, :secondname, :string
+      add_column :users, :role, :string
+  end
+end
