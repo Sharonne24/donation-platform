1f5a46c55c2857341de7fab3222d0bd4baaa7de4
Sharonne24 <sawuor395@gmail.com>
2023-10-02 23:31:58 +0300

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   resources :charities, only: [:show]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  delete '/logout', to: 'sessions#destroy'
 end
