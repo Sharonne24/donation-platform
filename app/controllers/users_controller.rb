18916f9116e8f98d4afeb770d4f5c32f258cb5b0
Sharonne24 <sawuor395@gmail.com>
2023-10-04 09:26:03 +0300

    token signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :handle_unprocessable_entity
   def new
      @user = User.new
      respond_to do |format|
@@ -8,10 +9,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
+    user = User.create!(user_params)
     if user
       # redirect_to login_path, notice: 'Registration successful. You can now log in.'
-      render json: {message: "Signup success"}
+      payload={user_id: user.id}
+        token= JWT.encode(payload, ENV["JWT_SECRET"])
+        render json: {id: user.id, email: user.email, firstname: user.firstname, secondname: user.secondname, role: user.role, token: token}, status: :created
     else
       # render :new
       render json: {message: "Signup was not successful"}
@@ -22,5 +25,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.permit(:email, :password, :firstname, :secondname, :role)
+  end
+
+  def handle_unprocessable_entity(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
 end
