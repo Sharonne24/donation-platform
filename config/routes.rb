e07f6ddf3f20366aae085d58183f7d21d6bfd4fd
Sharonne24 <sawuor395@gmail.com>
2023-10-05 11:11:25 +0300

    charity controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'charities/apply'
+  get 'charities/edit'
+  get 'charities/update'
+  get 'charities/donors'
+  get 'charities/donations'
+  get 'charities/stories'
+  get 'charities/beneficiaries'
+  get 'charities/automated_donations'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
--- db/migrate/20231004085242_add_image_to_charities.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToCharities < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :charities, :image, null: false, foreign_key: true
-  end
-end
