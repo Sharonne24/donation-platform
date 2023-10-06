c564153f5d4fb30b56cddd13063d6ebe080759f0
Sharonne24 <sawuor395@gmail.com>
2023-10-06 14:37:08 +0300

    trial

--- db/migrate/20231005092958_create_donations.rb
+++ db/migrate/20231005092958_create_donations.rb
@@ -1,7 +1,7 @@
 class CreateDonations < ActiveRecord::Migration[7.0]
   def change
     create_table :donations do |t|
-      t.references :donors, null: false, foreign_key: true
+      t.references :donor, null: false, foreign_key: true
       t.references :charity, null: false, foreign_key: true
       t.decimal :amount
       t.boolean :recurring
