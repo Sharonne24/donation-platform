4ddc4d972a43a8fb2d5b75b1dbfe2eaa545d29bc
nurdinhajo <nurdinhajo@gmail.com>
2023-10-13 19:04:30 +0300

    Resolve - Merge Conflict - routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,4 @@
 Rails.application.routes.draw do
-  # get 'charities/apply'
-  # get 'charities/edit'
-  # get 'charities/update'
-  # get 'charities/donors'
-  # get 'charities/donations'
-  # get 'charities/stories'
-  # get 'charities/beneficiaries'
-  # get 'charities/automated_donations'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-   get 'users/new'
-    get 'users/create'
-
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   resources :charities, only: [:show]
@@ -26,36 +9,28 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
 
-<<<<<<< HEAD
-   # Donations Routes
-   resources :donations
+  # Donations Routes
+  resources :donations
 
-   # Beneficiaries Routes
-   resources :beneficiaries
- 
-   # Admin Routes
-   resources :admin
- 
-   # Stories Routes
-   resources :stories
- 
-   # Additional custom routes
-   # e.g., route for donating to a specific charity
-   post '/charities/:id/donate', to: 'charities#donate', as: 'donate_to_charity'
- 
-=======
-  # apply to become a charity
-  post '/charities/apply', to: 'charities#apply'
-  # view charity details
-  get '/charities/:id', to: 'charities#show'
-  get '/charities', to: 'charities#index'
-  # edit charity details
-  get '/charities/:id/edit', to: 'charities#edit'
-  # update charity details
-  put '/charities/:id/update', to: 'charities#update'
-  patch '/charities/:id/update', to: 'charities#update'
+  # Beneficiaries Routes
+  resources :beneficiaries
 
-   devise_for :users
+  # Admin Routes
+  resources :admin
 
->>>>>>> main
+  # Stories Routes
+  resources :stories
+
+  # Additional custom routes
+  # e.g., route for donating to a specific charity
+  post '/charities/:id/donate', to: 'charities#donate', as: 'donate_to_charity'
+
+  # Routes related to charity management
+  resources :charities do
+    post 'apply', on: :member
+    get 'edit', on: :member
+    put 'update', on: :member
+  end
+
+  devise_for :users
 end
