102457df4ea28145a3b712532201f6350baaa9dd
Sharonne24 <sawuor395@gmail.com>
2023-10-16 16:44:16 +0300

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
      include ActionController::Cookies
-
-    
-#      skip_before_action :verify_authenticity_token
-#      include ActionController::Cookies
+     before_action :authorize_paypal_api_request!
    before_action :authenticate_user
+
+
     SECRET_KEY = ENV["JWT_SECRET"]
     # ALGORITHM = 'HS256'.freeze
   
@@ -35,19 +34,11 @@ class ApplicationController < ActionController::Base
         render json: {error: 'missing token'}, status: :unauthorized 
       end 
     end
-  end
-  
-
-#     skip_before_action :verify_authenticity_token
-#     include ActionController::Cookies
-
-    before_action :authorize_paypal_api_request!
-
-    private
-  
+    
     def authorize_paypal_api_request!
       Paypal::Authentication::BaseConfiguration.call(
         client_id: "AbOYC78JgkXVaLNHIfpr9Wif74EYXv_gOxNh4qM0A2KjGTT-Uj5rYkgzzmKK9yXX79I9Rch2iVP0E03L",
         secret_key: "EKM9JVryB3I80Dgi2IKFKR-AUmM1X1LJhxgux5obv7VH2MI9zDgBq2sudZp8yssPGNdLeGCUj5GEpZtN")
     end
-end
+  end
+  
